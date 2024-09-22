c1f5bb880fceb35f2fc472c341015f9ad2955407
Pakdee H <pakdee.hn@outlook.com>
2024-09-22 19:47:40 +0700

    [*] Update variable & class name

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import tkinter as tk
 
-class VidRec:
+class TRec:
     def __init__(self, window):
         self.window = window
         window.title('VidRec')
         window.geometry("400x100") 
         self.isRecord = False
-        self.data_record = []       
+        self.video_frames = [] 
+        self.audio_frames = []      
 
-        self.start_btn = tk.Button(window, text='Start', width=10, command=self.start_record)
+        self.start_btn = tk.Button(window, text='Start', width=10, command=self.start_recording)
         self.start_btn.grid(row=1, column=1, padx=5, pady=10)
 
-        self.stop_btn = tk.Button(window, text='Stop', width=10, command=self.stop_record)
+        self.stop_btn = tk.Button(window, text='Stop', width=10, command=self.stop_recording)
         self.stop_btn.grid(row=1, column=2, padx=5)
 
-        self.save_btn = tk.Button(window, text='Save', width=10, command=self.save_record)
+        self.save_btn = tk.Button(window, text='Save', width=10, command=self.save_recording)
         self.save_btn.grid(row=1, column=3, padx=5)
 
-    def start_record(self):
+    def start_recording(self):
         self.isRecord = True
         self.start_btn.config(state=tk.DISABLED)
         self.stop_btn.config(state=tk.ACTIVE)
 
-    def stop_record(self):
+    def stop_recording(self):
         self.isRecord = False
         self.stop_btn.config(state=tk.DISABLED)
         self.start_btn.config(state=tk.ACTIVE)
 
-    def save_record(self):
+    def save_recording(self):
         self.start_btn.config(state=tk.ACTIVE)
         self.stop_btn.config(state=tk.ACTIVE)
-        self.data_record = []
+        self.video_frames = []
+        self.audio_frames = []
 
 if __name__ == "__main__":
     window = tk.Tk()
-    VidRec(window)
+    TRec(window)
     window.mainloop()
